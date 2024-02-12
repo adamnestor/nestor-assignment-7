d14234f42d26712cc167024f76b6a0a23046db8e
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-12 17:12:43 -0500

    Update CustomArrayList.java

--- src/com/coderscampus/customarray/CustomArrayList.java
+++ src/com/coderscampus/customarray/CustomArrayList.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
-	private int size = 0; 
+	private int size = 0;
 
 // add method
 	@Override
 	public boolean add(T item) {
-		ensureCapacity(); 
+		ensureCapacity();
 
-		items[size] = item; 
-		size++; 
+		items[size] = item;
+		size++;
 
 		return true;
 	}
@@ -20,7 +20,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 // add to specific index method
 	@Override
 	public boolean add(int index, T item) throws IndexOutOfBoundsException {
-		
+
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException("Index out of bounds: " + index);
 		}
@@ -28,14 +28,13 @@ public class CustomArrayList<T> implements CustomList<T> {
 		ensureCapacity();
 		size++;
 		for (int i = index; i < size; i++) {
-			items[index+1] = items[index];
+			items[index + 1] = items[index];
 		}
 		items[index] = item;
-		
+
 		return true;
 	}
-	
-	
+
 // getSize Method
 	@Override
 	public int getSize() {
@@ -50,37 +49,34 @@ public class CustomArrayList<T> implements CustomList<T> {
 		}
 
 		@SuppressWarnings("unchecked")
-		T element = (T) items[index]; 
+		T element = (T) items[index];
 		return element;
 	}
 
 // remove Method
 	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
-		
+
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException("Index out of bounds: " + index);
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		T element = (T) items[index];
 
 		for (int i = index; i < size; i++) {
-			items[index] = items[index+1];
+			items[index] = items[index + 1];
 		}
 		size--;
-		
+
 		return element;
 	}
-	
-	
+
 // Method created to assist with add method	
 	private void ensureCapacity() {
-		if (size == items.length) { 			
-			items = Arrays.copyOf(items, items.length*2);
+		if (size == items.length) {
+			items = Arrays.copyOf(items, items.length * 2);
 		}
 	}
-
-
 
 }
